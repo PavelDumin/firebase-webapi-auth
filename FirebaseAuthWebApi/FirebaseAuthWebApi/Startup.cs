92c3e7b0c15b7f3e74bc6dba928fe225476ca629
Pavel.D <paveldumin@gmail.com>
2019-08-29 16:24:48 +0300

    Add FirebaseAuthWebApi solution

---
+++ FirebaseAuthWebApi/FirebaseAuthWebApi/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(FirebaseAuthWebApi.Startup))]
+namespace FirebaseAuthWebApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration httpConfiguration = new HttpConfiguration();
+
+            //Setup Firebase Authentication
+            FirebaseAuthConfig.Register(app);
+
+            WebApiConfig.Register(httpConfiguration);
+
+            app.UseWebApi(httpConfiguration);
+        }
+    }
+}
